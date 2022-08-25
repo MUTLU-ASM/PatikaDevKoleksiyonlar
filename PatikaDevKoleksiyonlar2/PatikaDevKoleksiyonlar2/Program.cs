554bc85a9eba1c352b907865838a7be4ec4c1d2d
Asım <asimmutlu6134@gmail.com>
2022-08-25 19:03:22 +0300

    PatikaDevKoleksiyonlar
    Generic Koleksiyonlar ve List

---
+++ PatikaDevKoleksiyonlar2/PatikaDevKoleksiyonlar2/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaDevKoleksiyonlar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<T> class
+            //System.Collections.Generic
+            //T-> object türündedir.
+
+            List<int> sayiListesi = new List<int>();
+            sayiListesi.Add(23);
+            sayiListesi.Add(10);
+            sayiListesi.Add(4);
+            sayiListesi.Add(5);
+            sayiListesi.Add(92);
+            sayiListesi.Add(34);
+
+            List<string> renkListesi = new List<string>();
+            renkListesi.Add("Kırmızı");
+            renkListesi.Add("Mavi");
+            renkListesi.Add("Turuncu");
+            renkListesi.Add("Sarı");
+            renkListesi.Add("Yeşil");
+
+            //Count = Eleman sayisini getirir.
+            Console.WriteLine(renkListesi.Count);
+            Console.WriteLine(sayiListesi.Count);
+
+            //Foreach ve List.ForEach ile eleman erişim 
+            foreach (var sayi in sayiListesi)
+                Console.WriteLine(sayi);
+            foreach (var renk in renkListesi)
+                Console.WriteLine(renk);
+
+            sayiListesi.ForEach(sayi => Console.WriteLine(sayi));
+            renkListesi.ForEach(renk => Console.WriteLine(renk));
+
+            //Listeden eleman cikarma
+            sayiListesi.Remove(4);
+            renkListesi.Remove("Yeşil");
+
+            sayiListesi.RemoveAt(0);
+            renkListesi.RemoveAt(1);
+
+            sayiListesi.ForEach(sayi => Console.WriteLine(sayi));
+            renkListesi.ForEach(renk => Console.WriteLine(renk));
+
+            //Liste içerisinde arama
+            if (sayiListesi.Contains(10))
+                Console.WriteLine("10 Liste içerisinde bulundu");
+
+            //Eleman ile index'e erişme
+            Console.WriteLine(renkListesi.BinarySearch("Sarı"));
+
+            //Diziyi List'e cevirme
+            string[] hayvanlar = { "Kedi", "Köpek", "Kuş" };
+            List<string> hayvanListesi = new List<string>(hayvanlar);
+
+            //Listeyi nasıl Temizleriz?
+            hayvanListesi.Clear();
+
+            //List icerisine nesne tutmak
+            List<Kullanicilar> kullanıcıListesi = new List<Kullanicilar>();
+            Kullanicilar kullanici1 = new Kullanicilar();
+            kullanici1.Isim = "Ayşe";
+            kullanici1.Soyisim = "Yılmaz";
+            kullanici1.Yas = 26;
+            
+            Kullanicilar kullanici2 = new Kullanicilar();
+            kullanici2.Isim = "Özcan";
+            kullanici2.Soyisim = "Çalışkan";
+            kullanici2.Yas = 26;
+
+            kullanıcıListesi.Add(kullanici1);
+            kullanıcıListesi.Add(kullanici2);
+
+            List<Kullanicilar> yeniListe = new List<Kullanicilar>();
+            yeniListe.Add(new Kullanicilar()
+            {
+                Isim = "Deniz",
+                Soyisim = "Arda",
+                Yas = 24
+            });
+            foreach (var kullanici in kullanıcıListesi)
+            {
+                Console.WriteLine("Kullanıcı Adı : "+kullanici.Isim);
+                Console.WriteLine("Kullanıcı Soyadı : "+kullanici.Soyisim);
+                Console.WriteLine("Kullanıcı Yaşı : "+kullanici.Yas);
+
+            }
+            yeniListe.Clear();
+
+        }
+    }
+
+    public class Kullanicilar
+    {
+        string isim;
+
+        string soyisim;
+
+        int yas;
+
+        public string Isim { get => isim; set => isim = value; }
+
+        public string Soyisim { get => soyisim; set => soyisim = value; }
+
+        public int Yas { get => yas; set => yas = value; }
+    }
+}
